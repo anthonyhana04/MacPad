257985fd542e65ffe15af88da64b31164c95e9d3
Anthony Hana <anthonyhana04@gmail.com>
2025-04-27 17:50:32 -0400

    Initial Commit

---
+++ MacPad/MacPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPadApp.swift
+//  MacPad
+//
+//  Created by anthony hana on 2025-04-27.
+//
+
+import SwiftUI
+
+@main
+struct MacPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacPadTests/MacPadTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacPadTests.swift
+//  MacPadTests
+//
+//  Created by anthony hana on 2025-04-27.
+//
+
+import Testing
+@testable import MacPad
+
+struct MacPadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
