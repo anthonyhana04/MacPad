8465467ec40b1ceebf079ac5b83bd0b270ca110a
Anthony Hana <anthonyhana04@gmail.com>
2025-05-04 02:48:27 -0400

    fixed file struct

--- Sources/App/Document.swift
+++ Sources/App/Document.swift
@@ -1,15 +1,14 @@
-// Sources/App/Document.swift
 import Foundation
 
 struct Document: Identifiable, Equatable {
-    var id       = UUID()
-    var text     = ""
+    var id = UUID()
+    var text = ""
     var fileURL: URL? = nil
-    var isDirty  = false
-    var workingName: String = "Untitled"
-
-    var displayName: String {
-        fileURL?.lastPathComponent ?? workingName
+    var isDirty = false
+    var workingName = "Untitled"
+    var displayName: String { fileURL?.lastPathComponent ?? workingName }
+    var hasUnsavedChanges: Bool {
+        isDirty || (fileURL == nil && !text.isEmpty)
     }
 }
 
